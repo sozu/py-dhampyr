f36eeab97b5f7e0585ea2f324c86990d97fa41f2
sozu <sozuberry@gmail.com>
2019-04-29 22:29:48 +0900

    Fix wrong library informations in setup.py

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@ with open("README.md", "r") as f:
 
 setup(
     name = "dhampyr",
-    version = "0.0.1",
+    version = "0.0.1.2",
     author = "sozuberry",
     author_email = "sozuberry@gmail.com",
     description = "Python simple validator for dict-like objects",
     long_description = long_description,
     long_description_content_type = "text/markdown",
-    url = "https://github.com/sozu/dhampyr",
+    url = "https://github.com/sozu/py-dhampyr",
     packages = find_packages(),
     classifiers = [
-        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
