ea2cb1876abb2f4d8653d8606c3ba48c8b592547
sozu <sozuberry@gmail.com>
2019-04-30 16:28:46 +0900

    Update library version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name = "dhampyr",
-    version = "0.0.1.2",
+    version = "0.0.2.0",
     author = "sozuberry",
     author_email = "sozuberry@gmail.com",
     description = "Python simple validator for dict-like objects",
